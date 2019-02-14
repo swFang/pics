33d5d4904ad5a4a422ab57989c3eeeac9e52da86
j0i1b <j0i1b@ugrad.cs.ubc.ca>
2019-02-13 23:51:17 -0800

    working on api calls

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className = "ui container" style = {{marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {query: term},
+      headers: {
+        Authorization:
+          "Client-ID d04f4c2ffd3c08f445b9d391da82ad1aade9efb36c083bffdb315a33ba284ed9"
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
