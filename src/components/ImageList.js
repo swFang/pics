16ce6d662840d42362afd17282d39dbf3ab036f1
j0i1b <j0i1b@ugrad.cs.ubc.ca>
2019-02-26 17:43:36 -0800

    finished refactor, need to fix css and sizing

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from 'react'; 
 
 const ImageList = (props) => {
-    const images = props.images.map((image) => {
-        return <img src={image.urls.regular} alt = "images" />
+    const images = props.images.map(({description, id, urls}) => {
+        return <img src={urls.regular} alt = {description} key ={id}  />
     });
     return <div> {images} </div>;
 }
